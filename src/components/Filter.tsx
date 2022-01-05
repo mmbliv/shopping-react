71707fa0fcb9ebf936e81bcbdef1a6666e509d3b
Min <mmbliv@gmail.com>
2022-01-05 11:03:40 -0800

    unique filter item helper setted

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 import { useFilterContext } from '../context/filter_context'
+import { getUniqueFilterItem } from '../utils/helper'
+
 
 const Filter = () => {
-    const { filters, searchProducts, cleanFilter } = useFilterContext()
+    const { filters, searchProducts, cleanFilter, all_products } = useFilterContext()
+    const categoryItems = getUniqueFilterItem(all_products, 'category')
+    const colorItems = getUniqueFilterItem(all_products, 'colors')
+    const companyItems = getUniqueFilterItem(all_products, 'company')
     return (
         <Wrapper>
             <form onSubmit={(e) => e.preventDefault()} >
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,7 +1,18 @@
+import { productsType } from "../context/products_context"
 export const formatPrice=(number:number)=>{
     return new Intl.NumberFormat('en-US',{
         style:'currency',
         currency:'USD',
 
     }).format(number/100)
+}
+type keyType='category'|'colors'|'company'
+export const getUniqueFilterItem=(data:productsType[],key:keyType)=>{
+    let uniqueItem=data.map((item)=>item[key])
+    if(key==='colors'){
+        uniqueItem=uniqueItem.flat()
+    }
+
+    return ['ALL',...new Set(uniqueItem)]
+
 }
