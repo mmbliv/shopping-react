882eee6ca66be95993cc425423f3acd12576bc4c
Min <mmbliv@gmail.com>
2022-01-19 13:48:45 -0800

    before set checkout context

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,26 @@ import './index.css';
 import App from './App';
 import { ProductsProvider } from './context/products_context';
 import { FilterProvider } from './context/filter_context';
-import { CartProvider } from './context/cart_context'
-
-
+import { CartProvider } from './context/cart_context';
+import { Auth0Provider } from "@auth0/auth0-react";
+// dev-ab6t-9cf.us.auth0.com
+//  RtfcjQrVoa0Tid8B1ngsfY74kgVIdiZW
 
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain='dev-ab6t-9cf.us.auth0.com'
+    clientId='RtfcjQrVoa0Tid8B1ngsfY74kgVIdiZW'
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>,
   document.getElementById('root')
 );
 
